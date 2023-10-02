79d799816507881f2f8beec2da2b099328b002a2
BanjiBear <tommychen900217@gmail.com>
2023-10-03 01:39:01 +0800

    [Class] rename Button class to Buttons / replace button_list with classmethod buttons:Dict

--- package/widgets/button.py
+++ package/widgets/button.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Dict, List
 from enum import Enum
 from PyQt6 import QtCore
 from PyQt6.QtGui import QFont
@@ -16,14 +16,15 @@ class Layout(Enum):
 	QFormLayout = QFormLayout()
 
 
-class Button:
+class Buttons:
+
+	buttons: Dict[str, QPushButton] = {}
 
 	def __init__(self, toolkit_width, toolkit_height):
 		self.toolkit_width = toolkit_width
 		self.toolkit_height = toolkit_height
 
 	def create_button(self, layout: str, button_label : List):
-		button_list = []
 		self.layout = Layout[layout].value
 		self.layout.setSpacing(10)
 		self.layout.setContentsMargins(0, 0, 0, 0)
@@ -35,30 +36,32 @@ class Button:
 			button.setFont(QFont('Arial', 30))
 			button.setFixedSize(QtCore.QSize(40, 40))
 			# Reference: https://stackoverflow.com/questions/56975249/button-resizing-automatically
-			button_list.append(button)
+			self.update_buttons(button)
 			# print(button.text())
 
 		if layout == "QGridLayout":
 			x = 0
 			y = 0
-			for b in button_list:
+			for b in self.buttons:
 				if y >= int(EnvSetting.ENV[constant.NUMBER_OF_BUTTONS_IN_ROW]):
 					x = x + 1
 					y = 0
-				# print(x, y, b.text())
-				self.layout.addWidget(b, x, y)
+				# print(x, y, buttons[b].text())
+				self.layout.addWidget(self.buttons[b], x, y)
 				y = y + 1
 		elif layout == "QFormLayout":
-			patch = 0
-			for i in len(button_list) / int(EnvSetting.ENV[constant.NUMBER_OF_BUTTONS_IN_ROW]):
-				...
-				layout.addRow(b for b in tmp)
+			# layout.addRow(b for b in tmp)
+			...
 		else:
 			self.layout.addStretch(1)
-			for b in button_list:
-				self.layout.addWidget(b)
+			for b in self.buttons:
+				self.layout.addWidget(self.buttons[b])
 
 		return self.layout
+
+	@classmethod
+	def update_buttons(cls, button):
+		cls.buttons.update({button.text():button})
 
 
 
