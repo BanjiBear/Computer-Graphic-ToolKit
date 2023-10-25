c5aba9e1277f8bed83a3fb206addf7ed02b3ed05
BanjiBear <tommychen900217@gmail.com>
2023-10-26 00:29:53 +0800

    [Function] apply args to pass in env files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 from PyQt6 import QtGui
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
@@ -11,8 +12,14 @@ from package.util.util import EnvSetting
 
 if __name__ == "__main__":
 
+	parser = argparse.ArgumentParser(description="Computer Graphics ToolKit")
+	parser.add_argument("--env", required=True, help="Mandate, the environment used")
+
+	args = parser.parse_args()
+	env = args.env
+
 	# System Setting
-	EnvSetting.read_env_file("./env/prd.env")
+	EnvSetting.read_env_file("./env/" + env + ".env")
 	
 	# Initialize the Canvas
 	canvas = Canvas()
